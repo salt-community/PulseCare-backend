c153226efc7bc9010670035a87781c6b40ef9f3a
Nora Silfver <nora.silfver@gmail.com>
2025-12-30 15:42:00 +0100

    feat: add HealthStatsController with response dto
    Co-authored-by: Marcus Grönnå <mgronna@gmail.com>
    Co-authored-by: Cristoffer Dessezar <dessezar@outlook.com>

---
+++ PulseCare.Api/Controllers/HealthStatsController.cs
@@ -0,0 +1,51 @@
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PulseCare.API.Context;
+using PulseCare.API.Data.Enums;
+
+namespace PulseCare.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HealthStatsController : ControllerBase
+{
+    private readonly PulseCareDbContext _context;
+    public HealthStatsController(PulseCareDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<List<HealthStatResponseDto>>> GetHealthStats(Guid id)
+    {
+        var healthData = _context.HealthStats.Where(h => h.PatientId == id);
+
+        var response = new List<HealthStatResponseDto>();
+
+        foreach (var item in healthData)
+        {
+            response.Add(new HealthStatResponseDto
+                (
+                    item.Type,
+                    item.Value,
+                    item.Unit,
+                    item.Date,
+                    item.Status
+                )
+            );
+        }
+
+        return response;
+    }
+}
+
+public record HealthStatResponseDto
+(
+    HealthStatType Type,
+    string Value,
+    string Unit,
+    DateTime Date,
+    [property: JsonConverter(typeof(JsonStringEnumConverter))]
+    HealthStatusType Status
+);
