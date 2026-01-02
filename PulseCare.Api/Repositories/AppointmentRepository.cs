f7777dba18e6202e17c6f9f57086d06724b99852
Johan Janérs <johanjaners@gmail.com>
2026-01-02 12:06:17 +0100

    feat: implement CRUD operations for appointments in AppointmentRepository

--- PulseCare.Api/Repositories/AppointmentRepository.cs
+++ PulseCare.Api/Repositories/AppointmentRepository.cs
@@ -1,10 +1,79 @@
+using Microsoft.EntityFrameworkCore;
+using PulseCare.API.Context;
 using PulseCare.API.Data.Entities.Medical;
 
 public class AppointmentRepository : IAppointmentRepository
 {
-    public Task<IEnumerable<Appointment>> GetAppointmentsById(Guid id)
+    private readonly PulseCareDbContext _context;
+
+    public AppointmentRepository(PulseCareDbContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
+    }
+    public async Task<IEnumerable<Appointment>> GetAppointmentsById(Guid patientId)
+    {
+        return await _context.Appointments
+            .Include(a => a.Patient)
+                .ThenInclude(p => p.User)
+            .Include(a => a.Doctor)
+                .ThenInclude(d => d.User)
+            .Include(a => a.AppointmentNotes)
+            .Where(a => a.PatientId == patientId)
+            .ToListAsync();
     }
 
+    public async Task<IEnumerable<Appointment>> GetAllAppointments()
+    {
+        return await _context.Appointments
+            .Include(a => a.Patient)
+                .ThenInclude(p => p.User)
+            .Include(a => a.Doctor)
+                .ThenInclude(d => d.User)
+            .Include(a => a.AppointmentNotes)
+            .ToListAsync();
+    }
+
+    public async Task<Appointment?> GetAppointmentById(Guid id)
+    {
+        return await _context.Appointments
+            .Include(a => a.Patient)
+                .ThenInclude(p => p.User)
+            .Include(a => a.Doctor)
+                .ThenInclude(d => d.User)
+            .Include(a => a.AppointmentNotes)
+            .FirstOrDefaultAsync(a => a.Id == id);
+    }
+
+    public async Task<Appointment> CreateAppointment(Appointment appointment)
+    {
+        _context.Appointments.Add(appointment);
+        await _context.SaveChangesAsync();
+        return appointment;
+    }
+
+    public async Task<Appointment?> UpdateAppointment(Appointment appointment)
+    {
+        var existingAppointment = await _context.Appointments.FindAsync(appointment.Id);
+        if (existingAppointment == null)
+        {
+            return null;
+        }
+
+        _context.Entry(existingAppointment).CurrentValues.SetValues(appointment);
+        await _context.SaveChangesAsync();
+        return existingAppointment;
+    }
+
+    public async Task<bool> DeleteAppointment(Guid id)
+    {
+        var appointment = await _context.Appointments.FindAsync(id);
+        if (appointment == null)
+        {
+            return false;
+        }
+
+        _context.Appointments.Remove(appointment);
+        await _context.SaveChangesAsync();
+        return true;
+    }
 }
--- PulseCare.Api/Repositories/IRepositories/IAppointmentRepository.cs
+++ PulseCare.Api/Repositories/IRepositories/IAppointmentRepository.cs
@@ -3,4 +3,9 @@ using PulseCare.API.Data.Entities.Medical;
 public interface IAppointmentRepository
 {
     Task<IEnumerable<Appointment>> GetAppointmentsById(Guid id);
+    Task<IEnumerable<Appointment>> GetAllAppointments();
+    Task<Appointment?> GetAppointmentById(Guid id);
+    Task<Appointment> CreateAppointment(Appointment appointment);
+    Task<Appointment?> UpdateAppointment(Appointment appointment);
+    Task<bool> DeleteAppointment(Guid id);
 }
