8c44cdc62289b98573645ad6c1baf55fd7aa134d
shakana0 <shakanajamal3@gmail.com>
2026-01-06 21:49:20 +0100

    reafactor: update method

--- PulseCare.Api/Hubs/CustomUserIdProvider.cs
+++ PulseCare.Api/Hubs/CustomUserIdProvider.cs
@@ -5,7 +5,9 @@ public class CustomUserIdProvider : IUserIdProvider
 {
     public string? GetUserId(HubConnectionContext connection)
     {
-        return connection.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value
-               ?? connection.User?.FindFirst("sub")?.Value;
+        var userId = connection.User?.FindFirst("sub")?.Value
+                     ?? connection.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        return userId;
     }
 }
