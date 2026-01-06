42f867d44113913b3ac7bb91fa2786cd60f32680
shakana0 <shakanajamal3@gmail.com>
2026-01-06 20:12:21 +0100

    refactor: update IMessageRepository

--- PulseCare.Api/Repositories/IRepositories/IMessageRepository.cs
+++ PulseCare.Api/Repositories/IRepositories/IMessageRepository.cs
@@ -3,5 +3,5 @@ public interface IMessageRepository
 {
     Task<Message> CreateMessageAsync(Guid conversationId, string subject, string content, bool fromPatient);
     Task<List<Message>> GetMessagesAsync(Guid conversationId);
-    Task<bool> MarkMessageAsReadAsync(Guid messageId, Guid conversationId);
+    public Task<bool> MarkAllAsReadAsync(Guid conversationId);
 }
