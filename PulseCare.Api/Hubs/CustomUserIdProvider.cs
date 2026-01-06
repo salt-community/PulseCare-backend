ef50c34d4487942d6ccdb4b824acbb51ad9671b7
shakana0 <shakanajamal3@gmail.com>
2026-01-06 20:14:57 +0100

    feat: add CustomUserIdProvider

---
+++ PulseCare.Api/Hubs/CustomUserIdProvider.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.SignalR;
+using System.Security.Claims;
+
+public class CustomUserIdProvider : IUserIdProvider
+{
+    public string? GetUserId(HubConnectionContext connection)
+    {
+        return connection.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value
+               ?? connection.User?.FindFirst("sub")?.Value;
+    }
+}
